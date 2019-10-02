e2e011c7c95a7aff3367536962c718953b324df4
Bas Bas <basriumar12@gmail.com>
2019-10-02 09:50:25 +0700

    add date utils

--- app/src/main/java/com/imastudio/guanzhuli/icart/utils/Constans.kt
+++ app/src/main/java/com/imastudio/guanzhuli/icart/utils/Constans.kt
@@ -10,3 +10,31 @@ object  Constans {
     val harga = "harga"
     val stock = "stock"
 }
+
+
+///add date utils
+object DateUtils {
+    const val DATE_FORMAT_YYYY = "yyyy-MM-dd"
+    const val DATE_FORMAT_FULL = "EEEE dd MMM yyyy"
+    const val DATE_FORMAT_HISTORY = "dd MMMM yyyy"
+
+    fun formatStringToDate(date: String, format: String) : Date{
+        val sdf = SimpleDateFormat(format, Locale.getDefault())
+        return sdf.parse(date)
+    }
+
+    fun formatDateLong(date: Long, destinationFormat: String) : String{
+        val sdfDest = SimpleDateFormat(destinationFormat, Locale.getDefault())
+        return sdfDest.format(Date(date))
+    }
+
+    fun formatDateLongLocale(date: Long, destinationFormat: String) : String{
+        val sdfDest = SimpleDateFormat(destinationFormat, Locale("in","ID"))
+        return sdfDest.format(Date(date))
+    }
+    fun formatDateLocale(date: Date, destinationFormat: String) : String{
+        val sdfDest = SimpleDateFormat(destinationFormat, Locale("in","ID"))
+        return sdfDest.format(date)
+    }
+
+}
